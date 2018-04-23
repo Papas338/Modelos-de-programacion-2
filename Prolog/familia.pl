9f2d872e1df1d8cec59a36ea47a5d3dd3ec4563a
Papas338 <31448314+Papas338@users.noreply.github.com>
2018-04-23 17:31:02 -0500

    Update familia.pl

--- Prolog/familia.pl
+++ Prolog/familia.pl
@@ -1,4 +1,37 @@
-padrede('Daniel','Jhonathan').
+padrede('Leonardo','Brandon').
+padrede('Sandra','Brandon').
+padrede('Leonardo','Lorena').
+padrede('Sandra','Lorena').
+padrede('Leonardo','Sebastian').
+padrede('Sandra','Sebastian').
+padrede('Teresa','Leonardo').
+padrede('Hector','Leonardo').
+padrede('Teresa','German').
+padrede('Hector','German').
+padrede('German','Julian').
+padrede('Lorenay','Julian').
+padrede('Reinaldo','Hector').
+padrede('Monica','Hector').
+padrede('Maria','Sandra').
+padrede('Oswaldo','Sandra').
+padrede('Oscar','Teresa').
+padrede('Susana','Teresa').
+padrede('Jose','Maria').
+padrede('Laura','Maria').
+padrede('Pablo','Oswaldo').
+padrede('Ana','Oswaldo').
+padrede('Maria','Deyanira').
+padrede('Oswaldo','Deyanira').
+padrede('Deyanira','Cristian').
+padrede('Ricardo','Cristian').
+padrede('Deyanira','Lis').
+padrede('Ricardo','Lis').
+padrede('Maria','Armando').
+padrede('Oswaldo','Armando').
+padrede('Armando','Oscar').
+padrede('Rut','Oscar').
+padrede('Armando','Julio').
+padrede('Rut','Julio').
 
 hijode(A,B) :- padrede(B,A).
 abuelode(A,B) :- padrede(A,C), padrede(C,B).
@@ -8,3 +41,4 @@ tiode(A,B) :- padrede(C,B), hermanode(A,C).
 bisabuelode(A,B) :- abuelode(A,C), padrede(C,B).
 nietode(A,B) :- abuelode(B,A).
 sobrinode(A,B) :- tiode(B,A).
+bisnietode(A,B) :- bisabuelode(B,A).
